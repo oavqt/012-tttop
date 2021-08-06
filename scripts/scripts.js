e4ae24392c6958b34fccb111e4a81c077261de67
oav <ortizqtv@gmail.com>
2021-08-05 20:10:02 -0500

    Add a pub-sub pattern function to allow different modules to communicate with each other simultaneously; moreover, make the board use this function to push x's and o's onto the array

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,52 @@
 //Tic Tac Toe
 
+const pubsub = (() => {
+  let pubsub = {};
+
+  function subscribe(evt, fn) {
+    pubsub[evt] = pubsub[evt] || [];
+    pubsub[evt].push(fn);
+  }
+
+  function unsubscribe(evt, fn) {
+    if (pubsub[evt]) {
+      for (let u = 0; u < pubsub[evt].length; u++) {
+        if (pubsub[evt][u] === fn) {
+          pubsub[evt].splice(u, 1);
+          break;
+        }
+      }
+    }
+  }
+
+  function publish(evt, data) {
+    if (pubsub[evt]) {
+      pubsub[evt].forEach((fn) => {
+        fn(data);
+      });
+    }
+  }
+
+  return { subscribe, unsubscribe, publish };
+})();
+
 const display = (() => {
-  let person = 'O';
+  let person = '';
 
   //cache DOM
-  const cards = [...document.querySelectorAll('.board__card')];
+  const squares = [...document.querySelectorAll('.board__square')];
   const scores = [...document.querySelectorAll('.player__score')];
   const btns = [...document.querySelectorAll('.btn')];
   const message = document.querySelector('.message__current');
 
   //Functions
+  function square() {
+    if (this.textContent !== '') this.disabled;
+    else this.textContent = person;
+
+    pubsub.publish('board', squares);
+  }
+
   function btn() {
     if (this.className === 'btn btn--o') {
       person = 'O';
@@ -31,7 +68,7 @@ const display = (() => {
       btns[0].disabled = false;
       btns[1].disabled = false;
 
-      cards.forEach((item) => {
+      square.forEach((item) => {
         item.textContent = '';
       });
 
@@ -39,23 +76,30 @@ const display = (() => {
     }
   }
 
-  function card() {
-    if (this.textContent !== '') this.disabled;
-    else this.textContent = person;
-
-    message.textContent = '';
-  }
-
   //Events
+  squares.forEach((item) => {
+    item.addEventListener('click', square);
+  });
+
   btns.forEach((item) => {
     item.addEventListener('click', btn);
   });
-
-  cards.forEach((item) => {
-    item.addEventListener('click', card);
-  });
-
-  return cards;
 })();
 
+const board = (() => {
+  let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+
+  pubsub.subscribe('board', populate);
+
+  function populate(data) {
+    for (let u = 0; u < data.length; u++) {
+      board[data[u].dataset['row']][data[u].dataset['column']] =
+        data[u].textContent;
+    }
+  }
+})();
 //
